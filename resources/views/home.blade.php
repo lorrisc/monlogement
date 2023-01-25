d38806f1c96ba1da67535f7795519960438fd7f3
lorrisc <l.crappier@gmail.com>
2023-01-25 11:20:35 +0100

    front end skeleton

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+<!-- css file -->
+@section('style')
+@endsection
+
+<!-- title page -->
+@section('title')
+Accueil
+@endsection
+
+<!-- content -->
+@section('content')
+@endsection
+
+<!-- js file -->
+@section('script')
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,36 @@
+<!-- WEBSITE SKELETON -->
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta name="keywords" content="bien immobilier, acheter, louer, location immobilière, immobilier">
+    <meta name="description" content="Trouvez un appartement ou une maison en vente ou en location. Découvrez de nombreuses annonces immobilières francaise actualisée en temps réel.">
+    <meta name="author" content="Lorris Crappier">
+
+    <link href="css/style.css" rel="stylesheet">
+    @yield('style')
+
+    <title>@yield('title') | MonLogement</title>
+
+</head>
+
+<body>
+
+    @include('partials.header')
+
+    @yield('content')
+
+    @include('partials.footer')
+
+    <script src="js/main.js"></script>
+    @yield('javascript')
+
+</body>
+
+</html>
